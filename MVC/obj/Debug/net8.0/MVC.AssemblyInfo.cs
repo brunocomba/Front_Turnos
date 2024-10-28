a0b2edf978e44eac06186c02289b6e5f2e8ca1ee
Bruno Comba <brcomba9@gmail.com>
2024-10-27 23:16:08 -0300

    hola

--- MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a4e7268a8a2691eb8137d4a84c2b5aefdef140")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622fc18ebb1d3b244c33ae8bdd7645b9dfee8b0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
